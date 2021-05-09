85408b82b1f590d4927b77344806ff69e0fc5728
Luiz Gabriel <luizgabriel.info@gmail.com>
2021-05-09 20:57:28 -0300

    text area with y axis resize

--- components/TextArea.js
+++ components/TextArea.js
@@ -20,6 +20,6 @@ export default function TextArea({name, error, onChangeValue, ...rest}) {
 			name={name}
 			{...(onChangeValue ? ({onChange: (e) => onChangeValue(e.target.value)}) : {})}
 			{...rest}
-			className={`w-full bg-gray-100 bg-opacity-50 rounded border focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out ${error ? "border-red-700" : "border-gray-300"}`}/>
+			className={`w-full bg-gray-100 bg-opacity-50 rounded border focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 resize-y leading-6 transition-colors duration-200 ease-in-out ${error ? "border-red-700" : "border-gray-300"}`}/>
 	);
 }
