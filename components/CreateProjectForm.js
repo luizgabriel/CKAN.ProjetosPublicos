ab8cef1dbdb333691fa0e1cc394362e9502c7925
Luiz Gabriel <luizgabriel.info@gmail.com>
2021-05-24 16:54:32 -0300

    Added create progress bar transition

--- components/CreateProjectForm.js
+++ components/CreateProjectForm.js
@@ -40,7 +40,7 @@ function ProgressBar({value, rounded, className}) {
 
 	return (
 		<div className={`overflow-hidden h-2 text-xs flex ${rounded} bg-gray-200 ${className}`}>
-			<div style={{width: `${value * 100}%`}} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"/>
+			<div style={{width: `${value * 100}%`}} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500 transition-all"/>
 		</div>
 	);
 }
