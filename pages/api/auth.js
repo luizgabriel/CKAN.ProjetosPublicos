ce596bb00cb7d61e2e0fa395389f09c8cfa00c06
Luiz Gabriel <luizgabriel.info@gmail.com>
2021-05-29 23:36:00 -0300

    Added lang redirection

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,17 +1,30 @@
 import withSession from "../../utils/session";
 
+function translateCkanLangToInternalLang(ckanLang) {
+	if (ckanLang.startsWith("pt"))
+		return "pt-BR";
+	else if (ckanLang.startsWith("en"))
+		return "en";
+	else if (ckanLang.startsWith("es"))
+		return "es";
+
+	return "";
+}
+
 async function authenticate(req, res) {
-	const {apiKey, host} = req.query;
+	const {apiKey, host, lang} = req.query;
 	const auth = {
 		apiKey,
 		host
 	};
 
+	const internalLang = translateCkanLangToInternalLang(lang);
+
 	console.log({auth});
 	req.session.set("auth", auth);
 	await req.session.save();
 
-	res.redirect("/");
+	res.redirect("/" + internalLang);
 };
 
 export default withSession(authenticate);
